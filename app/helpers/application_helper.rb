0f56c942ee5e73d3bd594bdf76f0a9062526583d
Joshua Vaage <joshuavaage@gmail.com>
2013-08-11 12:26:38 -0700

    add crude placeholder logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Open Vintage"
+    base_title = "Open Vintage"
     if page_title.empty?
       base_title
     else
